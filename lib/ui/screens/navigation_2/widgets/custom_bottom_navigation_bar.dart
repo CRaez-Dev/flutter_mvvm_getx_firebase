da1b8838c814c0f1a677f912ede6aad8c18bf7c6
CRaez-Dev <craez.dev@gmail.com>
2024-03-07 19:13:59 -0500

    [Navigation - 2nd option]
    - Buttom navigation bar example, without ClipRRect

--- lib/ui/screens/navigation_2/widgets/custom_bottom_navigation_bar.dart
+++ lib/ui/screens/navigation_2/widgets/custom_bottom_navigation_bar.dart
@@ -11,40 +11,43 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
     return SafeArea(
       child: Container(
+        decoration: const BoxDecoration(          
+        color:  Color(0xFF17203A),
+          borderRadius: BorderRadius.all(
+             Radius.circular(25),            
+          ),
+        ),
         margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Obx(
-          () => ClipRRect(
-            borderRadius: BorderRadius.circular(25),
-            
-            child: BottomNavigationBar(
-              elevation: 0,
-              backgroundColor: const Color(0xFF17203A),
-              currentIndex: _.currentIndex.value,
-              onTap: _.updateCurrentIndex,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              selectedItemColor: const Color(0xFF70B38F),
-              unselectedItemColor: const Color(0xFFACACE5),
-              type: BottomNavigationBarType.fixed,
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home_outlined),
-                  label: 'home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.folder_outlined),
-                  label: 'folder',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person_outline),
-                  label: 'person_outline',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.pets),
-                  label: 'pets',
-                ),
-              ],
-            ),
+          () => BottomNavigationBar(
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+            currentIndex: _.currentIndex.value,
+            onTap: _.updateCurrentIndex,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: const Color(0xFF70B38F),
+            unselectedItemColor: const Color(0xFFACACE5),
+            type: BottomNavigationBarType.fixed,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.folder_outlined),
+                label: 'folder',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: 'person_outline',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.pets),
+                label: 'pets',
+              ),
+            ],
           ),
         ),
       ),
